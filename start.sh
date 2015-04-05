f975e5b087f90f71b8eabcaad1a918dcd0dd823f
John Pradeep <yehohanan7@Johns-MacBook-Pro.local>
2015-04-05 22:24:18 +0100

    fixed mounting issue

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-docker run -v $(pwd):/sbcl -i -t -p 4005:4005 yehohanan7/clisp bash
+docker run -i -t -p 4005:4005 yehohanan7/clisp bash
 
--- swank.sh
+++ swank.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd):/sbcl -i -t -p 4005:4005 yehohanan7/clisp swank
+docker run -i -t -p 4005:4005 yehohanan7/clisp swank
